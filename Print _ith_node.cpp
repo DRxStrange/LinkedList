55ce696e1220656e57010cafb9f02a584c0a8782
Ashwani Sharma <107782712+DRxStrange@users.noreply.github.com>
2023-05-10 17:52:18 +0530

    Print _ith_node.cpp

---
+++ Print _ith_node.cpp
@@ -0,0 +1,94 @@
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+void printIthNode(Node *head, int i)
+{
+    //Write your code here
+	if(head ==  NULL) return ;
+
+	 int count =0;
+	 Node *temp = head;
+	  while(temp != NULL){
+		  if(count==i){
+			  cout<<temp->data;
+			  return;
+		  }else{
+			  temp= temp ->next;
+			  count++;
+		  }
+	  }
+
+
+}
+
+/*
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
+*/
