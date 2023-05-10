a7aba7b455709e70ce75d8dbe3984fcd7e34fe5e
Ashwani Sharma <107782712+DRxStrange@users.noreply.github.com>
2023-05-10 17:50:06 +0530

    lengthLL.cpp

---
+++ lengthLL.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+
+ /****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+int length(Node *head)
+{
+    //Write your code here
+	if(head==NULL){ 
+		return 0;
+	}
+	int count =0;
+	Node *temp =head;
+	while(temp != NULL){
+		temp = temp->next;
+		count++;
+	}
+	return count ;
+	
+}
+/*
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		cout << length(head) << endl;
+	}
+	return 0;
+}*/
